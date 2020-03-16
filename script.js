0a1508984c187ca6e1299f067e52916f4cc2736a
redDLADEK <chocovladik@gmail.com>
2020-03-16 17:04:32 +0200

    feat: add anchor scroll and slider

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+const menu = document.querySelector("nav ul");
 
+menu.addEventListener("click", (event) => {
+  menu.querySelectorAll('li button').forEach(e => e.classList.remove('current'));
+  event.target.classList.add("current");
+
+  let classForScroll = event.target.innerHTML.toLowerCase()+"";
+
+  window.scrollTo({
+    top: document.querySelector(`.${classForScroll}`).offsetTop - 0,
+    behavior: 'smooth'
+  });
+});
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
