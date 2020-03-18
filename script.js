a5834ed0c44752239d2ddd47c98fb353126f681c
redDLADEK <chocovladik@gmail.com>
2020-03-18 11:43:21 +0200

    feat: add portfolio shuffle

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const menu = document.querySelector("nav ul");
-
 menu.addEventListener("click", (event) => {
   menu.querySelectorAll('li button').forEach(e => e.classList.remove('current'));
   event.target.classList.add("current");
@@ -37,3 +36,37 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "flex";
 }
+
+function onoff1(id) {
+    var el = document.getElementById(id);
+    if (el.src.indexOf("assets/iPhone-Vertical.png")>0) {
+    el.src="assets/Phone Vertical.png"
+    }
+    else
+    {
+    document.getElementById(id).src="assets/iPhone-Vertical.png"
+    }
+}
+
+function onoff2(id) {
+    var el = document.getElementById(id);
+    if (el.src.indexOf("assets/iPhone-Horizontal.png")>0) {
+    el.src="assets/Phone Horizontal.png"
+    }
+    else
+    {
+    document.getElementById(id).src="assets/iPhone-Horizontal.png"
+    }
+}
+
+const tabs = document.querySelectorAll(".portfolio_tabs button");
+const portfolio = document.querySelector(".four_column_layout");
+tabs.forEach(tab => tab.addEventListener("click", (event) => {
+  if( !event.target.classList.contains("selected") ) {
+    let portfolioPictures = [...portfolio.querySelectorAll(".portfolio_picture")];
+    portfolioPictures.unshift(portfolioPictures.pop());
+    portfolioPictures.forEach( pic => portfolio.append(pic) );
+  }
+  tags.forEach(t => t.classList.remove('selected'));
+  event.target.classList.add("selected");
+}));
